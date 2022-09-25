424cbc8ba3307e419d61f1ab59d6710daeb5f6d7
jpdotcom <62865751+jpdotcom@users.noreply.github.com>
2022-09-25 10:28:00 -0400

    2022-09-25 10:28:00

---
+++ Auto_Upload_Codeforces/Codeforces_Round_726_Div_2/D_Deleting_Divisors.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define pb push_back
+#define FOR(b) for(int i=0;i<b;i++)
+typedef std::vector<int> vi;
+typedef std::pair<int,int> pi;
+#define mp(a,b) make_pair(a,b)
+typedef long long ll;
+typedef std::priority_queue<int> pq;
+#define endl '\n'
+#define F first 
+#define S second 
+#define PI 3.14159265358979323846
+#define print(x) cout<<x<<'\n'
+#define iset tree<pair<int,int>,null_type,less<pair<int,int>>,rb_tree_tag,tree_order_statistics_node_update>
+#define bit(x,s) ((x>>s)&1)
+using namespace std;
+void solve(){
+    int n; cin>>n;
+    if (n&1) print("Bob");
+    else if ((log2(n))==(int)log2(n)) print(((int)log2(n)&1 ? "Bob" : "Alice"));
+    else print("Alice");
+}
+void precompute(){
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    precompute();
+    int t=1;
+    cin>>t;
+    while (t--){
+        solve();
+    }
+}
+
+
