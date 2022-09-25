938c323cc6dd3f5a9ea853ddfed26432eb36254c
jpdotcom <62865751+jpdotcom@users.noreply.github.com>
2022-09-25 19:18:25 -0400

    2022-09-25 19:18:25

---
+++ Auto_Upload_Codeforces/Educational_Codeforces_Round_133_Rated_for_Div_2/A_2_-_3_Moves.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define pb push_back
+#define FOR(b) for(int i=0;i<(int)b;i++)
+#define mp(a,b) make_pair(a,b)
+typedef std::vector<int> vi;
+typedef std::pair<int,int> pi;
+typedef long long ll;
+#define F first 
+#define S second 
+#define PI 3.14159265358979323846
+#define print(x) cout<<x<<'\n'
+#define endl '\n'
+#define bit(x,s) ((x>>s)&1)
+#define oset tree<pair<int,int>, null_type,less<pair<int,int>>, rb_tree_tag,tree_order_statistics_node_update>
+#define INF ((ll)1e18+7)
+#define MOD ((ll)1e9+7)
+using namespace std;
+using namespace __gnu_pbds;
+void solve(){
+	int n; cin>>n;
+	if (n==1) print(2);
+	else if (n%3==0) print(n/3);
+	else if (n%3==2) print(n/3+1);
+	else print((n-4)/3+2);
+}
+void precompute(){
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout<<setprecision(15);
+    precompute();
+    int t=1;
+    cin>>t;
+    while (t--){
+        solve();
+    }
+}
