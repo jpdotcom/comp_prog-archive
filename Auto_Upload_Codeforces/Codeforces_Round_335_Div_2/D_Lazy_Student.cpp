df47c83a4a68cb47669bebde1528e89eebdf8a6b
jpdotcom <62865751+jpdotcom@users.noreply.github.com>
2022-09-25 17:56:18 -0400

    2022-09-25 17:56:18

---
+++ Auto_Upload_Codeforces/Codeforces_Round_335_Div_2/D_Lazy_Student.cpp
@@ -0,0 +1,167 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#define FOR(a,b) for (int i=a;i<b;++i)
+typedef std::pair<long long, long long> pi;
+typedef long long ll;
+#define F first 
+#define S second 
+using namespace std;
+ll modu(ll a,ll b){
+    return (b+(a%b)) % b;}
+ll root(ll x) {
+	ll l = 0, r = 1e9 + 1;
+	while (l < r - 1) {
+		ll m = (l + r) / 2;
+		if (m * m > x)
+			r = m;
+		else
+			l = m;
+	}
+	return l;
+}
+int power(long long x, unsigned int y, ll p)
+{
+    unsigned long long res = 1;     // Initialize result
+ 
+    x = x % p; // Update x if it is more than or
+                // equal to p
+  
+    if (x == 0) return 0; // In case x is divisible by p;
+ 
+    while (y > 0)
+    {
+        // If y is odd, multiply x with result
+        if (y & 1)
+            res = (res*x) % p;
+ 
+        // y must be even now
+        y = y>>1; // y = y/2
+        x = (x*x) % p;
+    }
+    return res;}
+
+
+const int N=5;
+int t[N];
+void build(int n) {  // build the tree
+  for (int i=2*N-1;i>=0;i--) t[i]=0;
+}
+
+void modify(int p, int value,int n) {  // set value at position p
+  for (t[p += n]=value ; p > 1; p >>= 1) t[p>>1] = t[p]+t[p^1];
+}
+
+int query(int l, int r,int n) {  // sum on interval [l, r)
+  int res = 0;
+  for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+    if (l&1) res+=t[l++];
+    if (r&1) res+=t[--r];
+  }
+  return res;
+}
+bool isPerfectSquare(ll x){
+    ll u=root(x);
+    return (u*u==x);
+}
+    
+void solve(){
+    priority_queue<pi> v;
+    int n,m;
+    cin>>n>>m;
+    for (int i=0;i<m;i++){
+        pi p;
+        cin>>p.F;
+        cin>>p.S;
+        p.F*=-1;
+        if (p.S==0){
+            p.S=-1*(i+1);
+        }
+        else{
+            p.S=i+1;
+        }
+        v.push(p);
+    }
+    int next_node=2;
+    int i=1;
+    int j=3;
+    int comp_size=1;
+    int last_comp_size=1;
+    pi ans[m];
+    int max_reach[n+1];
+    for (int i=0;i<n+1;i++){
+        max_reach[i]=i+1;
+    }
+    while (!v.empty()){
+        pi p = v.top();
+        v.pop();
+        int w=p.F;
+        int c=p.S;
+        pi add;
+        if (c>0){
+            add.F=next_node-1;
+            add.S=next_node;
+            ans[c-1]=add;
+            next_node++;
+            j=max_reach[1]+1;
+            i=1;
+            
+            comp_size++;
+            
+
+        }
+        else{
+            if (i>comp_size||j>comp_size){
+                cout<<-1<<endl;
+                return;
+            }
+            add.F=i;
+            add.S=j;
+            ans[-1*(c+1)]=add;
+            max_reach[i]=max(max_reach[i],j);
+            j++;
+            if (j>comp_size){
+                i++;
+                j=max_reach[i]+1;
+            }
+            
+        }
+    }
+    for (pi p: ans){
+        cout<<p.F<<' '<<p.S<<endl;
+    }
+    return;
+}
+        
+void precompute(){
+}
+
+
+int main(){
+    precompute();
+    int t=1;
+    for (int i=0;i<t;i++){
+        solve();
+    }
+}
+    
