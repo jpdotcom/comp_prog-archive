92430a94a545f53fbe05d0ca4f824f411d66ae26
jpdotcom <62865751+jpdotcom@users.noreply.github.com>
2022-09-25 10:27:56 -0400

    2022-09-25 10:27:56

---
+++ Auto_Upload_Codeforces/Codeforces_Round_807_Div_2/A_Mark_the_Photographer.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define pb push_back
+#define FOR(b) for(int i=0;i<b;i++)
+typedef std::vector<int> vi;
+typedef std::pair<int,int> pi;
+#define mp(a,b) make_pair(a,b)
+typedef long long ll;
+typedef std::priority_queue<int> pq;
+#define put push
+#define F first 
+#define S second 
+#define PI 3.14159265358979323846
+#define print(x) cout<<x<<'\n'
+#define iset tree<pair<int,int>,null_type,less<pair<int,int>>,rb_tree_tag,tree_order_statistics_node_update>
+#define bit(x,s) ((x>>s)&1)
+using namespace std;
+const int maxN=105;
+int a[maxN];
+void solve(){
+    int n,x; cin>>n>>x;
+    FOR(2*n) cin>>a[i];
+    sort(a,a+2*n);
+    int i=0;
+    int j=n;
+    while (j<2*n){
+        if (a[j]-a[i]>=x){
+            j++;
+            i++;
+        }
+        else j++;
+    }
+    if (i==n) print("YES");
+    else print("NO");
+}
+void precompute(){
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    precompute();
+    int t=1;
+    cin>>t;
+    while (t--){
+        solve();
+    }
+}
+
+
