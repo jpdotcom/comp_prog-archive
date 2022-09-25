cee490aa76fdaa120d0fdb4d2f2e716ce4e77a50
jpdotcom <62865751+jpdotcom@users.noreply.github.com>
2022-09-25 16:28:17 -0400

    2022-09-25 16:28:17

---
+++ Auto_Upload_Codeforces/Codeforces_Round_725_Div_3/F_Interesting_Function.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define pb push_back
+#define FOR(b) for(int i=0;i<b;i++)
+typedef std::vector<long long> vi;
+typedef std::pair<long long,long long> pi;
+#define mp(a,b) make_pair(a,b)
+typedef long long ll;
+typedef std::priority_queue<int> pq;
+#define endl '\n'
+#define F first 
+#define S second 
+#define PI 3.14159265358979323846
+#define print(x) cout<<x<<'\n'
+#define MOD (ll)(998244353)
+#define iset tree<pair<int,int>,null_type,less<pair<int,int>>,rb_tree_tag,tree_order_statistics_node_update>
+#define bit(x,s) ((x>>s)&1)
+using namespace std;
+using namespace __gnu_pbds;
+void solve(){
+    string a,b; cin>>a>>b;
+    string c="";
+    FOR((int)b.size()-(int)a.size()) c+='0';
+    a=c+a;
+    ll ans=0;
+    int n=b.size();
+    FOR(n){
+        ans+=stoi(b)-stoi(a);
+        b.pop_back();
+        a.pop_back();
+    }
+    print(ans);
+}
+void precompute(){
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    precompute();
+    int t=1;
+    cin>>t;
+    while (t--){
+        solve();
+    }
+}
