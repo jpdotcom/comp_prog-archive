0cf25d50048708886daa7ba76f5db63544e616aa
jpdotcom <62865751+jpdotcom@users.noreply.github.com>
2022-09-25 19:28:33 -0400

    2022-09-25 19:28:33

---
+++ Auto_Upload_Codeforces/Codeforces_Round_814_Div_2/A_Chip_Game.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define pb push_back
+#define FOR(b) for(int i=0;i<(int)b;i++)
+#define mp(a,b) make_pair(a,b)
+typedef std::vector<int> vi;
+typedef std::pair<int,int> pi;
+typedef long long ll;
+#define F first 
+#define S second 
+#define PI 3.14159265358979323846
+#define print(x) cout<<x<<'\n'
+#define endl '\n'
+#define bit(x,s) ((x>>s)&1)
+#define oset tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define INF ((int)1e9)
+#define MOD ((ll)1e9+7)
+using namespace std;
+const int maxN=1e5+1;
+void solve(){
+    int a,b; cin>>a>>b;
+    if (a==1 && b==1) print("Tonya");
+    else{
+        int v=((a-1)&1)+((b-1)&1);
+        if (v&1) print("Burenka");
+        else print("Tonya");
+    }
+}
+void precompute(){
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout<<setprecision(15);
+    precompute();
+    int t=1;
+    cin>>t;
+    while (t--)
+    {
+        solve();
+    }
+}
