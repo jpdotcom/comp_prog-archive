7467eb24451e6489b632cbd0041b76279f8bc58f
jpdotcom <62865751+jpdotcom@users.noreply.github.com>
2022-09-25 19:19:31 -0400

    2022-09-25 19:19:30

---
+++ Auto_Upload_Codeforces/Codeforces_Round_808_Div_2/A_Difference_Operations.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define pb push_back
+#define FOR(b) for(int i=0;i<b;i++)
+#define mp(a,b) make_pair(a,b)
+typedef std::vector<int> vi;
+typedef std::pair<int, int> pi;
+typedef long long ll;
+typedef std::priority_queue<int> pq;
+#define F first 
+#define S second 
+#define PI 3.14159265358979323846
+#define print(x) cout<<x<<'\n'
+#define endl '\n'
+#define bit(x,s) ((x>>s)&1)
+#define oset tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+using namespace std;
+using namespace __gnu_pbds;
+const int maxN=1e6+1;
+int a[maxN];
+void solve(){
+    int n; cin>>n;
+    FOR(n) cin>>a[i];
+    bool ok=false;
+    for (int i=1;i<n;i++){
+        ok|=(a[i]%a[0]);
+    }
+    if (ok) print("NO");
+    else print("YES");
+
+}
+void precompute(){
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout<<setprecision(15);
+    precompute();
+    int t=1;
+    cin>>t;
+    while (t--){
+        solve();
+    }
+}
+
+
