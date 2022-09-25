bb28bd533433b3ce345d6314c83f5d01a0643168
jpdotcom <62865751+jpdotcom@users.noreply.github.com>
2022-09-25 16:25:21 -0400

    2022-09-25 16:25:21

---
+++ Auto_Upload_Codeforces/Codeforces_Round_790_Div_4/A_Lucky.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define pb push_back
+#define FOR(b) for(int i=0;i<b;i++)
+typedef std::vector<int> vi;
+typedef std::pair<int,int> pi;
+#define mp(a,b) make_pair(a,b)
+typedef long long ll;
+typedef std::priority_queue<pi> pq;
+#define endl '\n'
+#define F first 
+#define S second 
+#define PI 3.14159265358979323846
+#define print(x) cout<<x<<'\n'
+#define MOD (int)1e9+7
+#define iset tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update>
+using namespace std;
+using namespace __gnu_pbds;
+ll modu(ll a,ll b){
+    return (b+(a%b)) % b;
+}
+ll power(ll x, ll y, ll p)
+{
+    ll res = 1;     
+    x = x % p; 
+    if (x == 0) return 0;
+    while (y > 0)
+    {
+        if (y & 1) res=modu(res*x,p);
+        y = y>>1; 
+        x = modu((x*x),p);
+    }
+    return res;
+}
+void fileIO(string filename) {
+	freopen((filename + ".in").c_str(), "r", stdin);
+	freopen((filename + ".out").c_str(), "w", stdout);
+}
+void solve(){
+    int n;
+    cin>>n;
+    int a=0,b=0;
+    FOR(3){
+        a+=(n%10);
+        n/=10;
+    }
+    FOR(3){
+        b+=(n%10);
+        n/=10;
+    }
+    if (a==b) print("YES");
+    else print("NO");
+}
+void precompute(){
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.precision(15);
+    precompute();
+    int t=1;
+    cin>>t;
+    while (t--){
+        solve();
+    }
+}
