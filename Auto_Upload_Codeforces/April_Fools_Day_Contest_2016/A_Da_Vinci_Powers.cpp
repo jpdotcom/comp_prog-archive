17ae82f759f9a324b16e436aa47c8f3bb698145e
jpdotcom <62865751+jpdotcom@users.noreply.github.com>
2022-09-25 16:21:10 -0400

    2022-09-25 16:21:10

---
+++ Auto_Upload_Codeforces/April_Fools_Day_Contest_2016/A_Da_Vinci_Powers.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define pb push_back
+#define FOR(b) for(int i=0;i<(int)b;i++)
+#define mp(a,b) make_pair(a,b)
+typedef std::vector<long long> vi;
+typedef long long ll;
+typedef std::pair<ll,ll> pi;
+#define F first 
+#define S second 
+#define PI 3.14159265358979323846
+#define endl '\n'
+#define print(x) cout<<x<<'\n'
+#define bit(x,s) ((x>>s)&1)
+#define oset tree<pair<int,int>, null_type,less<pair<int,int>>, rb_tree_tag,tree_order_statistics_node_update>
+#define INF ((ll)1e18+10)
+#define all(a) (a).begin(), (a).end()
+#define insert push
+#define MOD ((ll)1e9+7)
+using namespace std;
+using namespace __gnu_pbds;
+using u64 = uint64_t;
+using u128 = __uint128_t;
+const int maxN=2e5+1;
+void solve(){
+    int a; cin>>a;
+    if (a<=12) print((1<<a));
+    else{
+        ll val=8092;
+        a-=13;
+        while (a--) val*=2;
+        cout<<val<<endl;
+    }
+}
+void precompute(){
+}
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout<<setprecision(15);
+    precompute();
+    int t=1;
+    while (t--){
+        solve();
+        
+    }
+}
