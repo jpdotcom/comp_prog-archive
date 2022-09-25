53cc3cb3d9a8251d572640a11aefdd30ae845478
jpdotcom <62865751+jpdotcom@users.noreply.github.com>
2022-09-25 19:23:10 -0400

    2022-09-25 19:23:09

---
+++ Auto_Upload_Codeforces/Codeforces_Round_153_Div_2/C_Points_on_Line.py
@@ -0,0 +1,38 @@
+import math
+import random 
+from queue import Queue
+import time 
+
+
+
+def main(arr,d):
+
+  ans=0
+  for i in range(len(arr)):
+
+    start=i 
+    end=len(arr)-1
+    mid=(start+end)//2
+    best_val=mid
+    while start<=end:
+      
+      mid=(start+end)//2 
+     
+      if arr[mid]-arr[i]==d:
+        best_val=mid
+        
+        break 
+      elif arr[mid]-arr[i]>d:
+        
+        end=mid-1
+        
+      else:
+        start=mid+1
+        best_val=mid
+    
+    ans+=(best_val-i-1)*(best_val-i)//2 
+
+  return ans
+
+n,d=list(map(int,input().split()))
+print(main(list(map(int,input().split())),d)
