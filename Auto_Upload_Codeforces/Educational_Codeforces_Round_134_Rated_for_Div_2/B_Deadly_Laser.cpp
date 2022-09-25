951a37c5e02c306c70a9e1b6bf8b1ebcf136a557
jpdotcom <62865751+jpdotcom@users.noreply.github.com>
2022-09-25 18:55:11 -0400

    2022-09-25 18:55:11

---
+++ Auto_Upload_Codeforces/Educational_Codeforces_Round_134_Rated_for_Div_2/B_Deadly_Laser.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define pb push_back
+#define FOR(b) for(int i=0;i<(int)b;i++)
+#define mp(a,b) make_pair(a,b)
+typedef std::vector<int> vi;
+typedef std::pair<long long,long long> pi;
+typedef long long ll;
+#define F first 
+#define S second 
+#define PI 3.14159265358979323846
+#define print(x) cout<<x<<'\n'
+#define bit(x,s) ((x>>s)&1)
+#define oset tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define INF ((ll)1e18+7)
+#define MOD ((ll)1e9+7)
+using namespace std;
+const int maxN=1e6;
+void solve(){
+
+    int n,m,sx,sy,d; cin>>n>>m>>sx>>sy>>d;
+    if ((sx+d>=n && sy+d>=m) || (sx-d<=1 && sy-d<=1) || (sx-d<=1 && sx+d>=n) || (sy-d<=1 && sy+d>=m)) print(-1);
+    else print(n+m-2);
+}
+void precompute(){
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    precompute();
+    int t=1;
+    cin>>t;
+    while (t--) solve();
+}
