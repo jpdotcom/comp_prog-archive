79fecf7b4f38d0183f34c73d2c82ba95ab55e9f9
jpdotcom <62865751+jpdotcom@users.noreply.github.com>
2022-09-25 10:33:45 -0400

    2022-09-25 10:33:45

---
+++ Auto_Upload_Codeforces/Codeforces_Round_805_Div_3/A_Round_Down_the_Price.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define FOR(b) for (int i=0;i<b;i++)
+#define endl '\n';
+#define F first
+#define S second 
+#define mp make_pair
+#define print(x) cout<<x<<'\n'
+#define pb push_back 
+#define MOD (ll)1e9+7
+typedef std::vector<int> vi;
+typedef std::pair<long long, long long> pi;
+typedef long long ll;
+using namespace std;
+void solve(){
+    int n; cin>>n;
+    int ans=n-1;
+    for (int i=1;i<=n;i*=10){
+        ans=min(ans,n-i);
+    }
+    print(ans);
+
+}
+void precompute(){
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    precompute();
+    int t=1;
+    cin>>t;
+    while (t--){
+        solve();
+    }
+}
