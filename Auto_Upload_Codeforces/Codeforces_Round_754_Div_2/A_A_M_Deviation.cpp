b21a94f2fec942464585d576ef189037f653b9c4
jpdotcom <62865751+jpdotcom@users.noreply.github.com>
2022-09-25 16:11:49 -0400

    2022-09-25 16:11:49

---
+++ Auto_Upload_Codeforces/Codeforces_Round_754_Div_2/A_A_M_Deviation.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define pb push_back
+#define FOR(a) for(int i=0;i<a;i++)
+#define mp(a,b) make_pair(a,b)
+#define all(a)  a.begin(),a.end()
+typedef std::vector<long  long> vi;
+typedef std::pair<int,int> pi;
+typedef long long ll;
+#define F first 
+#define S second 
+#define PI 3.14159265358979323846
+#define print(x) cout<<x<<'\n'
+#define endl '\n'
+#define bit(x,s) ((x>>(s))&1)
+#define oset tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define INF ((int)1e9+10)
+#define MOD ((ll)1e9+7)
+using namespace std;
+using u64 = uint64_t;
+using u128 = __uint128_t;
+void solve(){
+    int a,b,c; cin>>a>>b>>c;
+    if (a+b>=2*c){
+        int diff=a+b-2*c;
+        print(min(diff%3,3-diff%3));
+    }
+    else{
+        int diff=2*c-a-b;
+        print(min(diff%3,3-diff%3));
+    }
+}
+void precompute(){
+}
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout<<setprecision(15);
+    precompute();
+    int t=1;
+    int i=1;
+    cin>>t;
+    while (t--)
+    {
+        solve();
+        ++i;
+    }
+}
