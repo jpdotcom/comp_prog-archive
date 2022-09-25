43d2fe55df726d827003a2fa81b523234c02bf1b
jpdotcom <62865751+jpdotcom@users.noreply.github.com>
2022-09-25 19:20:04 -0400

    2022-09-25 19:20:03

---
+++ Auto_Upload_Codeforces/Codeforces_Round_721_Div_2/C_Sequence_Pair_Weight.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define pb push_back
+#define FOR(b) for(int i=0;i<b;i++)
+typedef std::vector<long long> vi;
+typedef std::pair<long long,long long> pi;
+#define mp(a,b) make_pair(a,b)
+typedef long long ll;
+typedef std::priority_queue<int> pq;
+#define endl '\n'
+#define F first 
+#define S second 
+#define PI 3.14159265358979323846
+#define print(x) cout<<x<<'\n'
+#define MOD (ll)(1e9+7)
+#define iset tree<pair<int,int>,null_type,less<pair<int,int>>,rb_tree_tag,tree_order_statistics_node_update>
+#define bit(x,s) ((x>>s)&1)
+using namespace std;
+using namespace __gnu_pbds;
+const int maxN=1e5+1;
+int a[maxN];
+unordered_map<int, long long> cnt;
+void solve(){
+    cnt.clear();
+    int n; cin>>n;
+    FOR(n) cin>>a[i];
+    ll ans=0;
+    FOR(n){
+        if (cnt.find(a[i]) == cnt.end()) cnt[a[i]]=0;
+        ans+=(ll)(n-i)*(ll)cnt[a[i]];
+        cnt[a[i]]+=(i+1);
+    }
+    print(ans);
+}
+void precompute(){
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    precompute();
+    int t=1;
+    cin>>t;
+    while (t--){
+        solve();
+    }
+}
