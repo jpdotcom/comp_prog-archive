b37bc5e32aa7d3d8ea4e4e93815d13b69a2bd998
jpdotcom <62865751+jpdotcom@users.noreply.github.com>
2022-09-25 16:57:56 -0400

    2022-09-25 16:57:56

---
+++ Auto_Upload_Codeforces/Codeforces_Round_218_Div_2/B_Fox_Dividing_Cheese.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+map<pair<long long,long long>,long long>cache;
+
+int f(long long a,long long b){
+    
+    int val[4]={1,2,3,5};
+  
+    pair<long long,long long>p;
+    p=make_pair(a,b);
+    
+    if (a==b){
+        return 0;}
+    else if (cache.find(p)!=cache.end()){
+        return cache[p];}
+    else{
+        long long ans= 1000000000;
+        for (int i=0;i<4;i++){
+            for (int j=0;j<4;j++){
+                int val1,val2;
+               
+                val1=val[i];
+                val2=val[j];
+                if (val1==1 && val2==1){
+                    continue;}
+                
+                
+                else if (a%val1==0 && b%val2==0) {
+                    
+                    long long inc=(val1!=1 && val2!=1) ? 2:1;
+                    long long newa=a/val1;
+                    long long newb=b/val2;
+                    ans=min(ans,inc+f(newa,newb));
+                    
+
+                }
+
+                }}
+            cache[p]=ans;
+            cache[p]=ans;
+              return ans;}
+          }
+int main(){
+    long long a,b;
+    cin>>a;
+    cin>>b;
+    
+    long long res=f(a,b);
+   
+    if (res==1000000000){
+        cout<<-1;}
+    else{
+        cout<<res;}
+    return 0;
+}
+
