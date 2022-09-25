a5453feeb3b3937c42ce96138098ee249ef5ebc1
jpdotcom <62865751+jpdotcom@users.noreply.github.com>
2022-09-25 10:28:44 -0400

    2022-09-25 10:28:44

---
+++ Auto_Upload_Codeforces/Codeforces_Round_729_Div_2/C_Strange_Function.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define pb push_back
+#define FOR(b) for(int i=0;i<b;i++)
+typedef std::vector<long long> vi;
+typedef std::pair<int,int> pi;
+#define mp(a,b) make_pair(a,b)
+typedef long long ll;
+typedef std::priority_queue<int> pq;
+#define endl '\n'
+#define F first 
+#define S second 
+#define PI 3.14159265358979323846
+#define print(x) cout<<x<<'\n'
+#define MOD (ll)(1e9+7)
+#define iset tree<pair<int,int>,null_type,less<pair<int,int>>,rb_tree_tag,tree_order_statistics_node_update>
+#define bit(x,s) ((x>>s)&1)
+using namespace std;
+using namespace __gnu_pbds;
+const int maxN=3e5+1;
+void solve(){
+    ll n;
+    cin>>n;
+    ll v=1;
+    ll ans=0;
+    for (ll j=2;j<100;j++){
+        if (v>n) break;
+        ans+=(j*(n/v-n/lcm(v,j)))%MOD;
+        v=lcm(v,j);
+    } 
+    print(ans%MOD);
+}
+void precompute(){
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    precompute();
+    int t=1;
+    cin>>t;
+    while (t--){
+        solve();
+    }
+}
