2f1799b9ec12b5d16c32c46b2338ad5ab6f5d404
jpdotcom <62865751+jpdotcom@users.noreply.github.com>
2022-09-25 19:19:40 -0400

    2022-09-25 19:19:40

---
+++ Auto_Upload_Codeforces/Codeforces_Round_735_Div_2/B_Cobb.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define pb push_back
+#define FOR(b) for(int i=0;i<b;i++)
+typedef std::vector<int> vi;
+typedef std::pair<int,int> pi;
+#define mp(a,b) make_pair(a,b)
+typedef long long ll;
+typedef std::priority_queue<int> pq;
+#define endl '\n'
+#define F first 
+#define S second 
+#define PI 3.14159265358979323846
+#define print(x) cout<<x<<'\n'
+#define iset tree<pair<int,int>,null_type,less<pair<int,int>>,rb_tree_tag,tree_order_statistics_node_update>
+#define bit(x,s) ((x>>s)&1)
+using namespace std;
+using namespace __gnu_pbds;
+const int maxN=4e6+1;
+ll a[maxN];
+void solve(){
+    ll n,k; cin>>n>>k;
+    FOR(n) cin>>a[i];
+    ll ans=-1e18;
+    for (ll i=max(0LL,n-1-2*k);i<n;i++){
+        for (ll j=i+1;j<n;j++){
+            ans=max(ans,(i+1)*(j+1)-k*(a[i]|a[j]));
+        }
+    }
+    print(ans);
+
+}
+void precompute(){
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    precompute();
+    int t=1;
+    cin>>t;
+    while (t--){
+        solve();
+    }
+}
+
+
