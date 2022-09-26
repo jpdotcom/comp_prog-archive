63834e896136d3ef1653af007c7f6961a717aecf
jpdotcom <62865751+jpdotcom@users.noreply.github.com>
2022-09-25 20:53:42 -0400

    2022-09-25 20:53:42

---
+++ Auto_Upload_Codeforces/Codeforces_Round_790_Div_4/C_Most_Similar_Words.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define pb push_back
+#define FOR(b) for(int i=0;i<b;i++)
+typedef std::vector<int> vi;
+typedef std::pair<int,int> pi;
+#define mp(a,b) make_pair(a,b)
+typedef long long ll;
+typedef std::priority_queue<pi> pq;
+#define endl '\n'
+#define F first 
+#define S second 
+#define PI 3.14159265358979323846
+#define print(x) cout<<x<<'\n'
+#define MOD (int)1e9+7
+#define iset tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update>
+using namespace std;
+using namespace __gnu_pbds;
+ll modu(ll a,ll b){
+    return (b+(a%b)) % b;
+}
+ll power(ll x, ll y, ll p)
+{
+    ll res = 1;     
+    x = x % p; 
+    if (x == 0) return 0;
+    while (y > 0)
+    {
+        if (y & 1) res=modu(res*x,p);
+        y = y>>1; 
+        x = modu((x*x),p);
+    }
+    return res;
+}
+void fileIO(string filename) {
+	freopen((filename + ".in").c_str(), "r", stdin);
+	freopen((filename + ".out").c_str(), "w", stdout);
+}
+void solve(){
+    int n,m;
+    cin>>n>>m;
+    string arr[n];
+    FOR(n) cin>>arr[i];
+    ll ans=1e9;
+    FOR(n){
+        for (int j=i+1;j<n;j++){
+            ll temp=0;
+            for (int k=0;k<m;k++){
+                ll a=(int)arr[i][k]-97;
+                ll b=(int)arr[j][k]-97;
+                temp+=abs(a-b);
+            }
+            ans=min(ans,temp);
+        }
+    }
+    print(ans);
+}
+void precompute(){
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.precision(15);
+    precompute();
+    int t=1;
+    cin>>t;
+    while (t--){
+        solve();
+    }
+}
