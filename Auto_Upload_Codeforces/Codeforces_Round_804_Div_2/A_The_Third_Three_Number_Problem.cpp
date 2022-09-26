a8933ccc05706830d2c499f854babea005521008
jpdotcom <62865751+jpdotcom@users.noreply.github.com>
2022-09-25 20:53:07 -0400

    2022-09-25 20:53:07

---
+++ Auto_Upload_Codeforces/Codeforces_Round_804_Div_2/A_The_Third_Three_Number_Problem.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define pb push_back
+#define FOR(b) for(int i=0;i<b;i++)
+typedef std::vector<long long> vi;
+typedef std::pair<long long,long long> pi;
+#define mp(a,b) make_pair(a,b)
+typedef long long ll;
+typedef std::priority_queue<int> pq;
+#define endl '\n'
+#define F first 
+#define S second 
+#define PI 3.14159265358979323846
+#define print(x) cout<<x<<'\n'
+#define MOD (ll)(998244353)
+#define iset tree<pair<int,int>,null_type,less<pair<int,int>>,rb_tree_tag,tree_order_statistics_node_update>
+#define bit(x,s) ((x>>s)&1)
+using namespace std;
+using namespace __gnu_pbds;
+const int maxN=1e5+1;
+void solve(){
+    int n; cin>>n;
+    if ((n&1)==0) cout<<n/2<<' '<<0<<' '<<0<<endl;
+    else print(-1);
+}
+void precompute(){
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    precompute();
+    int t=1;
+    cin>>t;
+    while (t--){
+        solve();
+    }
+}
