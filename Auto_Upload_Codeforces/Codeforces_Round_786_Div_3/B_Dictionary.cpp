daa8879285858eced5e33381ff4b90c6575a7fd6
jpdotcom <62865751+jpdotcom@users.noreply.github.com>
2022-09-25 20:53:47 -0400

    2022-09-25 20:53:47

---
+++ Auto_Upload_Codeforces/Codeforces_Round_786_Div_3/B_Dictionary.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define FOR(b) for(int i=0;i<b;i++)
+typedef std::vector<int> vi;
+typedef std::pair<int,int> pi;
+#define mp(a,b) make_pair(a,b)
+typedef long long ll;
+typedef std::priority_queue<pi> pq;
+#define endl '\n'
+#define F first 
+#define S second 
+#define PI 3.14159265358979323846
+#define print(x) cout<<x<<'\n'
+#define MOD 1e9+7;
+using namespace std;
+ll modu(ll a,ll b){
+    return (b+(a%b)) % b;
+}
+ll power(ll x, ll y, ll p)
+{
+    ll res = 1;     
+    x = x % p; 
+    if (x == 0) return 0;
+    while (y > 0)
+    {
+        if (y & 1) res=modu(res*x,p);
+        y = y>>1; 
+        x = modu((x*x),p);
+    }
+    return res;
+}
+void fileIO(string filename) {
+	freopen((filename + ".in").c_str(), "r", stdin);
+	freopen((filename + ".out").c_str(), "w", stdout);
+}
+const int maxN=1e5;
+void solve(){
+    string s;
+    cin>>s;
+    int v1=(int)s[0]-97;
+    int v2=(int)s[1]-97;
+    int idx=1;
+    for (int i=0;i<26;i++){
+        for (int j=0;j<26;j++){
+            if (j!=i && v1==i && v2==j){
+                print(idx);
+                return;
+            }
+            if (j!=i) idx++;
+        }
+    }
+}
+void precompute(){
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.precision(30);
+    precompute();
+    int t=1;
+    cin>>t;
+    while (t--){
+        solve();
+    }
+}
