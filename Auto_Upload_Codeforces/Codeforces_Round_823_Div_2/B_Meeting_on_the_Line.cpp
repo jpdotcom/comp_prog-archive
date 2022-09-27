52c60a1841e8d4834a07050992f50ee9700ada33
jpdotcom <62865751+jpdotcom@users.noreply.github.com>
2022-09-26 20:27:20 -0400

    2022-09-26 20:27:20

---
+++ Auto_Upload_Codeforces/Codeforces_Round_823_Div_2/B_Meeting_on_the_Line.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define pb push_back
+#define FOR(b) for(int i=0;i<b;i++)
+typedef std::vector<int> vi;
+typedef std::pair<long long,long long> pi;
+#define mp(a,b) make_pair(a,b)
+typedef long long ll;
+typedef std::priority_queue<int> pq;
+#define F first 
+#define S second 
+#define PI 3.14159265358979323846
+#define print(x) cout<<x<<'\n'
+#define MOD (ll)(998244353)
+#define INF ((int)1e9+7)
+#define iset tree<pair<int,int>,null_type,less<pair<int,int>>,rb_tree_tag,tree_order_statistics_node_update>
+#define bit(x,s) ((x>>s)&1)
+using namespace std;
+using namespace __gnu_pbds;
+using u64 = uint64_t;
+using u128 = __uint128_t;
+const int maxN=1e5+10;
+void solve(){
+
+    int v1,v2,n; cin>>n;
+    v1=-INF,v2=-INF;
+    vector<int>a(n),b(n);
+    FOR(n) cin>>a[i];
+    FOR(n) cin>>b[i];
+    FOR(n){
+        v1=max(v1,-1*a[i]+b[i]);
+        v2=max(v2,a[i]+b[i]);
+    }
+    print((v2-v1)/2.0);
+}
+void precompute(){
+}
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cout<<setprecision(15);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    precompute();
+    int t=1;
+    cin>>t;
+    while (t--){
+        solve();
+    }
+}
+
