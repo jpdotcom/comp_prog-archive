93d6c02976285dabdfa5fc61632ffecad5d29625
jpdotcom <62865751+jpdotcom@users.noreply.github.com>
2022-09-28 17:44:29 -0400

    2022-09-28 17:44:29

---
+++ Auto_Upload_AtCoder/abc257/abc257: A - A to Z String 2.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define pb push_back
+#define FOR(b) for(int i=0;i<b;i++)
+typedef std::vector<long long> vi;
+typedef std::pair<int,int> pi;
+#define mp(a,b) make_pair(a,b)
+typedef long long ll;
+typedef std::priority_queue<int> pq;
+#define endl '\n'
+#define F first 
+#define S second 
+#define PI 3.14159265358979323846
+#define print(x) cout<<x<<'\n'
+#define MOD (int)(1e9+7)
+#define iset tree<pair<int,int>,null_type,less<pair<int,int>>,rb_tree_tag,tree_order_statistics_node_update>
+#define bit(x,s) ((x>>s)&1)
+using namespace std;
+using namespace __gnu_pbds;
+const int maxN=1e5+1;
+void solve(){
+    int n,x; cin>>n>>x;
+    print((char)('A'+((x-1)/n)));
+}
+void precompute(){
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    precompute();
+    int t=1;
+    while (t--){
+        solve();
+    }
+}
