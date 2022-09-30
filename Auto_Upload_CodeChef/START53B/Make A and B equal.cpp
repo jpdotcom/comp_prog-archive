c6942c2a521a4d769d076755f676802ef94bb66d
jpdotcom <62865751+jpdotcom@users.noreply.github.com>
2022-09-30 19:14:02 -0400

    2022-09-30 19:14:02

---
+++ Auto_Upload_CodeChef/START53B/Make A and B equal.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define pb push_back
+#define FOR(b) for(int i=0;i<(int)b;i++)
+#define mp(a,b) make_pair(a,b)
+typedef std::vector<long  long> vi;
+typedef std::pair<int,int> pi;
+typedef long long ll;
+#define F first 
+#define S second 
+#define PI 3.14159265358979323846
+#define print(x) cout<<x<<'\n'
+#define endl '\n'
+#define bit(x,s) ((x>>(s))&1)
+#define oset tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define INF ((int)1e9)
+#define MOD ((ll)1e9+7)
+using namespace std;
+const int maxN=3e5+10;
+int n;
+int a[maxN];
+int b[maxN];
+void solve(){
+    cin>>n;
+    ll av=0,bv=0;
+    FOR(n) cin>>a[i];
+    FOR(n) cin>>b[i];
+    FOR(n){
+        av+=max(0,a[i]-b[i]);
+        bv+=max(0,b[i]-a[i]);
+    }
+    if (bv!=av) print(-1);
+    else print(av);
+}
+void precompute(){
+}
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout<<setprecision(15);
+    precompute();
+    int t=1;
+    cin>>t;
+    while (t--)
+    {
+        solve();
+    }
+}
