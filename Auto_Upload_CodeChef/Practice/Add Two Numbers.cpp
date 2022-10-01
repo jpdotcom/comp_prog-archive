b39b8769f744781fcb4319e4e4b195a81c1cc37a
jpdotcom <62865751+jpdotcom@users.noreply.github.com>
2022-10-01 19:07:00 -0400

    2022-10-01 19:07:00

---
+++ Auto_Upload_CodeChef/Practice/Add Two Numbers.cpp
@@ -0,0 +1,20 @@
+// You don't need to add/edit anything to the below solution. 
+// Click on the SUBMIT button to solve your first problem on CodeChef.
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+   int t;
+   cin>>t;
+   int a,b;
+   while(t--){
+    int sum=0;
+    cin>>a>>b;
+    sum=a+b;
+    cout<<sum<<"\n";
+   }
+   return 0;
+}
