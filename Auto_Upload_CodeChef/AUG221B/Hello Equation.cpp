a405665b201b0aba7d2d297b2e8706421f7d9e43
jpdotcom <62865751+jpdotcom@users.noreply.github.com>
2022-10-01 18:21:13 -0400

    2022-10-01 18:21:13

---
+++ Auto_Upload_CodeChef/AUG221B/Hello Equation.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define pb push_back
+#define FOR(b) for(int i=0;i<(int)b;i++)
+#define mp(a,b) make_pair(a,b)
+typedef std::vector<long long> vi;
+typedef std::pair<int,int> pi;
+typedef long long ll;
+#define F first 
+#define S second 
+#define PI 3.14159265358979323846
+#define endl '\n'
+#define print(x) cout<<x<<'\n'
+#define bit(x,s) ((x>>s)&1)
+#define oset tree<pair<int,int>, null_type,less<pair<int,int>>, rb_tree_tag,tree_order_statistics_node_update>
+#define INF ((ll)1e18)
+#define all(a) (a).begin(), (a).end()
+#define MOD ((ll)998244353)
+using namespace std;
+using namespace __gnu_pbds;
+const int maxN=1e5+1;
+int a[maxN];
+void solve(){
+    int X; cin>>X;
+    bool ok=false;
+    for (int i=1;i<=sqrt(X);i++){
+        int v=X-2*i;
+        if (v%(i+2)==0 && v/(i+2)>0) ok=true;
+    }
+    if (ok) print("YES");
+    else print("NO");
+}
+void precompute(){
+}
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout<<setprecision(15);
+    precompute();
+    int t=1;
+    cin>>t;
+    while (t--){
+        solve();
+        
+    }
+}
