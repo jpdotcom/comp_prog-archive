eb26aca48c8dc8d5a3307764e9c42b93ad826ede
jpdotcom <62865751+jpdotcom@users.noreply.github.com>
2022-10-01 18:12:03 -0400

    2022-10-01 18:12:03

---
+++ Auto_Upload_CodeChef/Practice/Chef and Coins Game.py
@@ -0,0 +1,17 @@
+import sys
+import math
+
+def main(n):
+    
+    if n%6==0:
+        return "Misha"
+    return "Chef"
+    
+    
+
+for i in range(int(input())):
+    
+    n=int(input())
+    print(main(n))
+
+        
