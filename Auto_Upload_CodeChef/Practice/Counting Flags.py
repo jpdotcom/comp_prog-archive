930e592e0a5d11e30f5c99dbe1b195be82b77caf
jpdotcom <62865751+jpdotcom@users.noreply.github.com>
2022-10-01 18:38:24 -0400

    2022-10-01 18:38:24

---
+++ Auto_Upload_CodeChef/Practice/Counting Flags.py
@@ -0,0 +1,15 @@
+import sys
+import math
+
+
+    
+    
+   
+
+     
+
+for _ in range(int(input())):
+    n=int(input())
+    print(n*(n-1)**2+n*(n-1)**2+n*(n-1)*(n-2)+n*(n-1)*(n-2)*(n-2)+n*(n-1)*(n-2)*(n-2))
+
+        
