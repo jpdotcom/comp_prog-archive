67e3ab0b4be78ab8e2d078a5b041f45f7c5955ac
jpdotcom <62865751+jpdotcom@users.noreply.github.com>
2022-10-01 18:42:28 -0400

    2022-10-01 18:42:28

---
+++ Auto_Upload_CodeChef/START47B/End Sorted.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define pb push_back
+#define FOR(b) for(int i=0;i<b;i++)
+typedef std::vector<int> vi;
+typedef std::pair<int,int> pi;
+#define mp(a,b) make_pair(a,b)
+typedef long long ll;
+typedef std::priority_queue<int> pq;
+#define endl '\n'
+#define F first 
+#define S second 
+#define PI 3.14159265358979323846
+#define print(x) cout<<x<<'\n'
+#define iset tree<pair<int,int>,null_type,less<pair<int,int>>,rb_tree_tag,tree_order_statistics_node_update>
+#define bit(x,s) ((x>>s)&1)
+using namespace std;
+using namespace __gnu_pbds;
+const int maxN=4e6+1;
+int a[maxN];
+void solve(){
+    int n; cin>>n;
+    int x=0,y=0;
+    FOR(n){
+        cin>>a[i];
+        if (a[i]==1) x=i;
+        if (a[i]==n) y=i;
+    }
+    if (x>y){
+        print(x+n-2-y);
+    }
+    else print(x+n-1-y);
+
+}
+void precompute(){
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    precompute();
+    int t=1;
+    cin>>t;
+    while (t--){
+        solve();
+    }
+}
+
+
