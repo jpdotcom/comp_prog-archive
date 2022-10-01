9cbacf675ad337d78c8b412674407a399d161e49
jpdotcom <62865751+jpdotcom@users.noreply.github.com>
2022-10-01 18:27:36 -0400

    2022-10-01 18:27:36

---
+++ Auto_Upload_CodeChef/Practice/Courses in an university.py
@@ -0,0 +1,21 @@
+import sys
+import math
+
+def main(arr):
+    
+    
+   
+    a=len(arr)-max(arr)
+    return a 
+
+for _ in range(int(input())):
+    n=int(input())
+    arr=list(map(int,input().split()))
+    print(main(arr))
+    
+    
+    
+    
+    
+
+        
