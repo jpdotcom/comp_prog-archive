c686a00326d10dd5e442eb55b6058c7fa274cee0
jpdotcom <62865751+jpdotcom@users.noreply.github.com>
2022-10-05 13:56:05 -0400

    2022-10-05 13:56:05

---
+++ Auto_Upload_Codeforces/Codeforces_Round_824_Div_2/D_Meta-set.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define pb push_back
+#define FOR(b) for(int i=0;i<b;i++)
+typedef std::vector<int> vi;
+typedef std::pair<long long,long long> pi;
+#define mp(a,b) make_pair(a,b)
+#define all(a) a.begin(),a.end()
+typedef long long ll;
+typedef std::priority_queue<int> pq;
+#define F first 
+#define S second 
+#define PI 3.14159265358979323846
+#define print(x) cout<<x<<'\n'
+#define MOD (ll)(998244353)
+#define INF ((int)1e9+7)
+#define iset tree<pair<int,int>,null_type,less<pair<int,int>>,rb_tree_tag,tree_order_statistics_node_update>
+#define bit(x,s) ((x>>s)&1)
+using namespace std;
+using namespace __gnu_pbds;
+using u64 = uint64_t;
+using u128 = __uint128_t;
+const int maxN=2e5+10;
+map<vector<int>,int> cnt;
+vector<int> arr[maxN];
+void solve(){
+    int n,k; cin>>n>>k;
+    cnt.clear();
+    set<vi> has;
+    for (int i=0;i<n;i++){
+        vector<int> c(k);
+        for (int j=0;j<k;j++) cin>>c[j];
+        arr[i]=c;
+        has.insert(c);
+    }
+    for (int i=0;i<n;i++){
+        for (int j=i+1;j<n;j++){
+            vector<int> c(k);
+            int s=0;
+            for (int l=0;l<k;l++){
+                if (arr[i][l]==arr[j][l]){
+                    c[l]=arr[i][l];
+                }
+                else c[l]=3-arr[i][l]-arr[j][l];
+                s+=c[l];
+            }
+            cnt[c]++;
+        }
+    }
+    ll ans=0;
+    for (auto &it: cnt){
+        if (has.find(it.F)!=has.end()) ans+=(ll)(it.S)*(it.S-1)/2;
+    }
+    print(ans);
+}
+void precompute(){
+}
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cout<<setprecision(15);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    precompute();
+    int t=1;
+    while (t--){
+        solve();
+    }
+}
+
