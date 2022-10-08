212faef0bb6c69056fbc591a53c501a6feb95280
jpdotcom <62865751+jpdotcom@users.noreply.github.com>
2022-10-08 17:36:09 -0500

    2022-10-08 18:36:08

---
+++ Auto_Upload_AtCoder/abc272/abc272: B - Everyone is Friends.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define pb push_back
+#define FOR(b) for(int i=0;i<b;i++)
+typedef std::vector<int> vi;
+typedef std::pair<long long,long long> pi;
+#define mp(a,b) make_pair(a,b)
+#define all(a) a.begin(),a.end()
+typedef long long ll;
+typedef std::priority_queue<int> pq;
+#define F first 
+#define S second 
+#define PI 3.14159265358979323846
+#define print(x) cout<<x<<'\n'
+#define MOD (ll)(998244353)
+#define INF ((int)1e9+7)
+#define iset tree<pair<int,int>,null_type,less<pair<int,int>>,rb_tree_tag,tree_order_statistics_node_update>
+#define bit(x,s) ((x>>s)&1)
+using namespace std;
+using namespace __gnu_pbds;
+using u64 = uint64_t;
+using u128 = __uint128_t;
+const int maxN=105;
+int has[maxN][maxN];
+void solve(){
+
+    int n,m; cin>>n>>m;
+    FOR(m){
+        int k; cin>>k;
+        for (int j=0;j<k;j++){
+            int p; cin>>p;
+            has[p][i]=1;
+        }
+    }
+    for (int i=1;i<n+1;i++){
+        for (int j=i+1;j<n+1;j++){
+            int cnt=0;
+            for (int k=0;k<m;k++){
+                cnt+=(has[j][k] && has[i][k]);
+            }
+            if (!cnt){
+                print("No");
+                return;
+            }
+        }
+    }
+    print("Yes");
+}
+void precompute(){
+}
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cout<<setprecision(15);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    precompute();
+    int t=1;
+    while (t--){
+        solve();
+    }
+}
+
